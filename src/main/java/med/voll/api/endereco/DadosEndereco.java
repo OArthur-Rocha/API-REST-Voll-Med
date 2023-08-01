652c4dda79543b124d0b1f1703c21396afe2169d
Arthur Rocha <arthur.oliveira182@hotmail.com>
2023-08-01 10:34:23 -0300

    crud concluído

---
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,24 @@
+package med.voll.api.endereco;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+        @NotBlank
+        String logradouro,
+        String numero,
+        String complemento,
+
+        @NotBlank
+        String bairro,
+
+        @NotBlank
+        String cidade,
+
+        @NotBlank
+        String uf,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep) {
+}
---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,65 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.bairro = dados.bairro();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.cep = dados.cep();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null) {
+            this.logradouro = dados.logradouro();
+        }
+
+        if (dados.numero() != null) {
+            this.numero = dados.numero();
+        }
+
+        if (dados.complemento() != null) {
+            this.complemento = dados.complemento();
+        }
+
+        if (dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+
+        if (dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+
+        if (dados.uf() != null) {
+            this.uf = dados.uf();
+        }
+
+        if (dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+
+    }
+
+
+    public void atualizarInformacoes(Endereco endereco) {
+    }
+}
