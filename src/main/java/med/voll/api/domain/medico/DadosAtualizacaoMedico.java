3f798ad77160fa2dcf8c723d1a9fbaf504137be8
Arthur Rocha <arthur.oliveira182@hotmail.com>
2023-08-08 22:47:25 -0300

    add boas práticas e segurança com Spring security

--- src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedico.java
@@ -1,7 +1,7 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.constraints.NotNull;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
 public record DadosAtualizacaoMedico(
         @NotNull
