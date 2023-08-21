b1d372c529da27468a0af4fd264e36af2d617399
Arthur Rocha <arthur.oliveira182@hotmail.com>
2023-08-20 22:02:04 -0300

    feat: add parte de consultas ao projeto

---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consulta;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+        Long idMedico,
+
+        @NotNull
+        @JsonAlias({"id_paciente", "paciente_id"})
+        Long idPaciente,
+
+        @NotNull @Future
+        LocalDateTime data,
+
+        Especialidade especialidade) {
+}
